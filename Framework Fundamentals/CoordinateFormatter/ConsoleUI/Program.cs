8ca10add13dc6a1d6937bae53261f2ddaa7d8113
yuriy-ulivanov-siemens <yuriy-ulivanov-siemens@github.com>
2020-07-21 00:33:34 +0300

    docs: add Messages.resx contains messages of the console UI

--- Framework Fundamentals/CoordinateFormatter/ConsoleUI/Program.cs
+++ Framework Fundamentals/CoordinateFormatter/ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoordinateFormatter;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +9,33 @@ namespace ConsoleUI
 {
     class Program
     {
+        /// <summary>
+        /// Main method.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Insert coordinate X: ");
+            Console.Write(Messages.CoordinateXIntro);
             var XStringFormat = Console.ReadLine();
 
+            Console.Write(Messages.CoordinateYIntro);
+            var YStringFormat = Console.ReadLine();
 
+            var parseXResult = double.TryParse(XStringFormat, out double CoordinateX);
+            var parseYResult = double.TryParse(YStringFormat, out double CoordinateY);
+
+            if (!parseXResult || !parseYResult)
+            {
+                Console.WriteLine(Messages.IncorrectlyInsert);
+                Console.ReadKey();
+                return;
+            }
+
+            // Create instance of Coordinate class.
+            var coordinate = new Coordinate(CoordinateX, CoordinateY);
+
+            Console.WriteLine(coordinate.ToString());
+            Console.ReadKey();
         }
     }
 }
