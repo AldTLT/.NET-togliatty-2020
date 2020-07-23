e4d22514638fc58cad71f7303e3be3ec34aaf8fb
yuriy-ulivanov-siemens <yuriy-ulivanov-siemens@github.com>
2020-07-23 20:50:15 +0300

    feat: add choise input data from file by console

--- Framework Fundamentals/CoordinateFormatter/ConsoleUI/Program.cs
+++ Framework Fundamentals/CoordinateFormatter/ConsoleUI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using CoordinateFormatter;
 using System;
+using System.IO;
 
 namespace ConsoleUI
 {
@@ -14,43 +15,101 @@ namespace ConsoleUI
         const int MAX_COORD_NUMBER = 100;
 
         /// <summary>
+        /// The maximum file size in bytes from which data input can be redirected. The size is selected arbitrarily.
+        /// </summary>
+        const long MAX_FILE_SIZE = 10000;
+
+        /// <summary>
         /// Main method.
         /// </summary>
         static void Main()
         {
-            var isInputRedirected = Console.IsInputRedirected;
-            // Count of coordinates to print. Initial value.
-            var coordCount = 0;
+            string inputData = string.Empty;
+            bool isInputDataCoordinates;
+            bool isInputDataFileName;
 
-            // Show intro messages.
-            Console.Clear();
-            Console.WriteLine(Messages.Title);
-            Console.WriteLine(Messages.EnterData);
-
+            // Repeating until the correct value is entered.
             do
             {
-                var inputData = Console.ReadLine();
+                Console.Clear();
+                // Show intro messages.
+                Console.WriteLine(Messages.Title);
 
-                if (inputData == null || coordCount > MAX_COORD_NUMBER)
+                if (Console.IsInputRedirected)
+                {
+                    isInputDataCoordinates = true;
+                    isInputDataFileName = false;
+                    break;
+                }
+
+                Console.Write(Messages.EnterData);
+
+                inputData = Console.ReadLine();
+                // Check if a input data is coordinate.
+
+                isInputDataCoordinates = CoordinateManager.IsCoordinates(inputData);
+                // Check if a input data is file name.
+                isInputDataFileName = CoordinateManager.IsFileName(inputData);
+
+                if (isInputDataCoordinates || isInputDataFileName)
                 {
                     break;
                 }
 
-                var parseResult = CoordinateManager.TryParse(inputData, out Coordinate coordinate);
-                var messageToInput = parseResult ? coordinate.ToString() : Messages.WrongCoordinate;
-                coordCount++;
-
-                Console.WriteLine(messageToInput);
-
-                if (!isInputRedirected)
-                {
-                    break;
-                }
+                Console.WriteLine(Messages.IncorrectlyInsert);
+                Console.ReadKey();
             }
-
             while (true);
 
+            // If file name entered - redirect input from file.
+            if (isInputDataFileName)
+            {
+                PrintByFileName(inputData);
+            }
+
+            // If coordinates entered - input from console.
+            if (isInputDataCoordinates)
+            {
+                PrintByCoordinates(inputData);
+            }
+
+            // Create standard reader from console.
+            var standardInput = new StreamReader(Console.OpenStandardInput());
+            Console.SetIn(standardInput);
             Console.ReadLine();
+        }
+
+        /// <summary>
+        /// The method prints data from file to console.
+        /// </summary>
+        /// <param name="fileName">Name of the file from which gets data to print.</param>
+        private static void PrintByFileName(string fileName)
+        {
+            var fileSize = FileManager.GetFileSize(fileName);
+            if (fileSize > MAX_FILE_SIZE)
+            {
+                Console.WriteLine($"{Messages.FileTooBig} {MAX_FILE_SIZE} bytes.");
+            }
+            else
+            {
+                FileManager.RedirectInputFromFile(fileName);
+            }
+        }
+
+        /// <summary>
+        /// The method prints coordinates to console.
+        /// </summary>
+        /// <param name="coordinates">Coordinate to print.</param>
+        private static void PrintByCoordinates(string coordinates)
+        {
+            if (Console.IsInputRedirected)
+            {
+                FileManager.PrintCoordinates(MAX_COORD_NUMBER);
+            }
+            else
+            {
+                FileManager.PrintCoordinate(coordinates);
+            }
         }
     }
 }
