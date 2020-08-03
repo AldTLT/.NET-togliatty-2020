f8a3007b65953a50d5cfdfc44261c00556619696
aldtlt <vr0rtex@mail.ru>
2020-08-03 22:57:05 +0300

    feat: add output result and result calculated by Math.Pow

--- Basic coding/NRoot/NRoot.ConsoleUI/Program.cs
+++ Basic coding/NRoot/NRoot.ConsoleUI/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using NRoot.Calculation;
+using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,72 @@ namespace NRoot.ConsoleUI
     {
         static void Main(string[] args)
         {
+            Console.WriteLine($"{Messages.Title}\n");
+            Console.WriteLine(Messages.Input_Value);
+            var valueToCalculate = GetValueFromConsole();
+
+            Console.WriteLine(Messages.Input_Root);
+            var root = GetValueFromConsole();
+
+            Console.WriteLine(Messages.Input_Accuracy);
+            var accuracy = GetValueFromConsole();
+
+            var nRoot = valueToCalculate.GetNRoot(root: root, accuracy: accuracy);
+
+            if (double.IsNaN(nRoot))
+            {
+                Console.WriteLine(Messages.HasNotRoot);
+            }
+            else
+            {
+                Console.WriteLine($"\n{Messages.NRootResult} {nRoot}\n{nRoot}^{root}={valueToCalculate}");
+
+                var valueByMath = Math.Pow(nRoot, root);
+                Console.WriteLine($"\n{Messages.MathPowMessage} {nRoot}^{root}={valueByMath}");
+            }
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// The method returns double value, entered by console.
+        /// </summary>
+        /// <returns>Double value.</returns>
+        private static double GetValueFromConsole()
+        {
+            double enteredValue;
+
+            do
+            {
+                enteredValue = EnterValue();
+
+                if (!double.IsNaN(enteredValue))
+                {
+                    break;
+                }
+
+                Console.WriteLine(Messages.TryAgain);
+            }
+            while (true);
+
+            return enteredValue;
+        }
+
+        /// <summary>
+        /// The method returns double value from console.
+        /// </summary>
+        /// <returns>Double value if entered value is correct, otherwise - double.NaN.</returns>
+        private static double EnterValue()
+        {
+            var currentCulture = CultureInfo.CurrentCulture;
+            var valueString = Console.ReadLine();
+            var parseResult = double.TryParse(
+                valueString,
+                NumberStyles.AllowDecimalPoint | NumberStyles.AllowExponent | NumberStyles.AllowLeadingSign,
+                currentCulture, out double value
+                );
+
+            return parseResult ? value : double.NaN;
         }
     }
 }
