1f47169b28c05b76d42ba264280d2ec9daa9f4c9
aldtlt <vr0rtex@mail.ru>
2020-08-05 00:27:27 +0300

    feat: add int to binary task

---
+++ Basic coding/IntToBinary/IntToBinary.Converter/Converter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntToBinary.Converter
+{
+    /// <summary>
+    /// The class represents methods of convertation from UInt32 to binary representation.
+    /// </summary>
+    public static class Converter
+    {
+        /// <summary>
+        /// Method returns a binary representation of a UInt32 number in string format.
+        /// </summary>
+        /// <param name="sourceNumber">UInt32 number to convert.</param>
+        /// <returns>Binary representation of the UInt32 number in string format.</returns>
+        public static string ToBinaryCustom(this uint sourceNumber)
+        {
+            if (sourceNumber == 0)
+            {
+                return sourceNumber.ToString();
+            }
+
+            var resultContainer = new StringBuilder();
+
+            while (sourceNumber > 0)
+            {
+                var bit = sourceNumber % 2;
+                sourceNumber >>= 1;
+
+                // Position to insert bit is 0.
+                const int indexOfPosition = 0;
+                // Number of digits to insert is 1.
+                const int numberToInsert = 1;
+
+                resultContainer.Insert(indexOfPosition, bit.ToString(), numberToInsert);
+            }
+
+            return resultContainer.ToString();
+        }
+
+        /// <summary>
+        /// Method returns a binary representation of a UInt32 number.
+        /// The method using standart classes and methods.
+        /// </summary>
+        /// <param name="sourceNumber">UInt32 number to convert.</param>
+        /// <returns>Binary representation of the UInt32 number in string format.</returns>
+        public static string ToBinaryStandart(this uint sourceNumber)
+        {
+            //The base of the return value for method Convert.ToString.
+            const int baseOfConvert = 2;
+            return Convert.ToString(sourceNumber, baseOfConvert);
+        }
+    }
+}
---
+++ Basic coding/IntToBinary/IntToBinary.UI/Program.cs
@@ -0,0 +1,42 @@
+﻿using IntToBinary.Converter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntToBinary.UI
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Messages.Title);
+            Console.Write($"\n{Messages.Intro} ");
+
+            uint numberToConvert;
+
+            do
+            {
+                var numberString = Console.ReadLine();
+                var parseResult = UInt32.TryParse(numberString, out numberToConvert);
+
+                if (parseResult)
+                {
+                    break;
+                }
+
+                Console.WriteLine(Messages.IncorrectNumber);
+            }
+            while (true);
+
+            var binaryCustom = numberToConvert.ToBinaryCustom();
+            var binaryStandart = numberToConvert.ToBinaryStandart();
+
+            Console.WriteLine($"\n{Messages.BinaryOutCustom} {binaryCustom}");
+            Console.WriteLine($"\n{Messages.BinaryOutStandart} {binaryStandart}");
+
+            Console.ReadKey();
+        }
+    }
+}
