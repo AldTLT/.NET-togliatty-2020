c556707c245c0502d76be20fd432fa7512012aaf
aldtlt <vr0rtex@mail.ru>
2020-08-04 23:05:02 +0300

    feat: add garbage collector task

---
+++ Garbage-collector/GarbageCollector/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GarbageCollector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "a";
+
+            while(true)
+            {
+                Console.WriteLine(s.Length);
+                s += s;
+            }
+        }
+    }
+}
