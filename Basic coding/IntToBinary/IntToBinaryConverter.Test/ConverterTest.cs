3218dc1a7c8592a80750b611d65629abdafed8b8
aldtlt <vr0rtex@mail.ru>
2020-08-05 21:12:58 +0300

    test: add test of ToBinaryStandart and ToBinaryCustom methods.

---
+++ Basic coding/IntToBinary/IntToBinaryConverter.Test/ConverterTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using IntToBinary.Converter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntToBinaryConverter.Test
+{
+    [TestClass]
+    public class ConverterTest
+    {
+        /// <summary>
+        /// The test of ToBinaryCustom method. The ones convert UInt32 to binary string format.
+        /// </summary>
+        /// <param name="valueToConvert">UInt32 value to convert.</param>
+        /// <param name="expectedBinaryValue">Expected binary of converted value.</param>
+        [DataTestMethod]
+        [DataRow((uint)0, "0")]
+        [DataRow((uint)2, "10")]
+        [DataRow(uint.MaxValue, "11111111111111111111111111111111")]
+        [DataRow(uint.MinValue, "0")]
+        [DataRow((uint)3455, "110101111111")]
+        [DataRow((uint)94395803, "101101000000101110110011011")]
+        public void ToBinaryCustom_BinaryRepresentation_Test(uint valueToConvert, string expectedBinaryValue)
+        {
+            var binaryValue = valueToConvert.ToBinaryCustom();
+
+            Assert.AreEqual(expectedBinaryValue, binaryValue);
+        }
+
+        /// <summary>
+        /// The test of ToBinaryStandart method. The ones convert UInt32 to binary string format.
+        /// </summary>
+        /// <param name="valueToConvert">UInt32 value to convert.</param>
+        /// <param name="expectedBinaryValue">Expected binary of converted value.</param>
+        [DataTestMethod]
+        [DataRow((uint)0, "0")]
+        [DataRow((uint)2, "10")]
+        [DataRow(uint.MaxValue, "11111111111111111111111111111111")]
+        [DataRow(uint.MinValue, "0")]
+        [DataRow((uint)3455, "110101111111")]
+        [DataRow((uint)94395803, "101101000000101110110011011")]
+        public void ToBinaryStandart_BinaryRepresentation_Test(uint valueToConvert, string expectedBinaryValue)
+        {
+            var binaryValue = valueToConvert.ToBinaryStandart();
+
+            Assert.AreEqual(expectedBinaryValue, binaryValue);
+        }
+    }
+}
---
+++ Basic coding/IntToBinary/IntToBinaryConverter.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IntToBinaryConverter.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IntToBinaryConverter.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c7439d59-b897-44d2-8d13-7a27ec64bca4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
