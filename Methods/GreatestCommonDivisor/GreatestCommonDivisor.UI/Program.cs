426323626a24a011b54a54ea5f050928e68b73db
aldtlt <vr0rtex@mail.ru>
2020-08-08 00:10:51 +0300

    init: add Greatest Common Divisor task.

---
+++ Methods/GreatestCommonDivisor/GreatestCommonDivisor.UI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatestCommonDivisor.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
