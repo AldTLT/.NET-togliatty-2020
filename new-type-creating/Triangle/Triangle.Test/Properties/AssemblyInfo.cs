927054da19ee1758f608ddad69b32f0100794438
Aldtlt <vr0rtex@mail.ru>
2020-08-12 22:37:03 +0300

    test: add tests of correct triangle creating, test of side is zero or less, test of side is too big.

---
+++ new-type-creating/Triangle/Triangle.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Triangle.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Triangle.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1cdc0c19-3945-4fc2-b61b-52c7313c0327")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ new-type-creating/Triangle/Triangle.Test/TriangleTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Triangle.Test
+{
+    [TestClass]
+    public class TriangleTest
+    {
+        [DataTestMethod]
+        [DataRow(10.0, 10.0, 10.0)]
+        [DataRow(124.356, 32.3, 98.0)]
+        [DataRow(23.7, 57.1, 40.5)]
+        public void CreateTriangle_Assert_Test(double sideA, double sideB, double sideC)
+        {
+            var triangle = new Triangle((decimal)sideA, (decimal)sideB, (decimal)sideC);
+
+            Assert.IsInstanceOfType(triangle, typeof(Triangle));
+        }
+
+        [DataTestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        [DataRow(0.0, 10.0, 10.0)]
+        [DataRow(124.356, 0.0, 98.0)]
+        [DataRow(23.7, 57.1, 0.0)]
+        [DataRow(2.0, -1.0, 2.0)]
+        [DataRow(-8.9, -22.1, -15.0)]
+        [DataRow(23.7, 37.1, -10.0)]
+        public void SidesIsZeroOrLess_Throw_Test(double sideA, double sideB, double sideC)
+        {
+            var triangle = new Triangle((decimal)sideA, (decimal)sideB, (decimal)sideC);
+        }
+
+        [DataTestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        [DataRow(1.0, 10.0, 15.0)]
+        [DataRow(124.356, 20.0, 90.0)]
+        [DataRow(10000.0, 57.1, 67.0)]
+        [DataRow(2.0, 20.0, 2.0)]
+        public void SidesIsTooBig_Throw_Test(double sideA, double sideB, double sideC)
+        {
+            var triangle = new Triangle((decimal)sideA, (decimal)sideB, (decimal)sideC);
+        }
+    }
+}
