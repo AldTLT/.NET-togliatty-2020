1b5ca4adf23441053553f34ff56a32e093ec4e3a
aldtlt <vr0rtex@mail.ru>
2020-08-14 12:45:08 +0300

    feat: add methods of calculate execution time

---
+++ Methods/GreatestCommonDivisor/GreatestCommonDivisor.Calculate/GCD.Calculate.Watch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace GreatestCommonDivisor.Calculate
+{
+    /// <summary>
+    /// The partial class represents methods of execution time of GCD calculation.
+    /// </summary>
+    public static partial class GcdCalculate
+    {
+        /// <summary>
+        /// The method returns GCD using Euclidean algorithm and outs execution time of calculation.
+        /// </summary>
+        /// <param name="firstNumber">First number to GCD calculate.</param>
+        /// <param name="secondNumber">Second number to GCD calculate.</param>
+        /// <param name="executionTime">Execution time of GCD calculation.</param>
+        /// <returns>Greatest common divisor.</returns>
+        public static int GetEuclideanWatch(int firstNumber, int secondNumber, out TimeSpan executionTime)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+
+            var gcdEuclidean = GcdCalculate.GetGcdEuclidean(firstNumber, secondNumber);
+
+            watch.Stop();
+            executionTime = watch.Elapsed;
+
+            return gcdEuclidean;
+        }
+
+        /// <summary>
+        /// The method returns GCD using Binary algorithm and outs execution time of calculation.
+        /// </summary>
+        /// <param name="firstNumber">First number to GCD calculate.</param>
+        /// <param name="secondNumber">Second number to GCD calculate.</param>
+        /// <param name="executionTime">Execution time of GCD calculation.</param>
+        /// <returns>Greatest common divisor.</returns>
+        public static int GetBinaryWatch(int firstNumber, int secondNumber, out TimeSpan executionTime)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+
+            var gcdEuclidean = GcdCalculate.GetGcdBinary(firstNumber, secondNumber);
+
+            watch.Stop();
+            executionTime = watch.Elapsed;
+
+            return gcdEuclidean;
+        }
+    }
+}
